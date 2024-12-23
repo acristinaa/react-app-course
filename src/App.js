785b30bfd5b40d8a6f1a59d531ca1dbb8ba73e51
Cristina Ayala <crisstinaayala777@gmail.com>
2024-12-23 20:16:19 +0900

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      
+      <Todo text='Hi'/>
+      <Todo text='How are you?'/>
+      <Todo text='I am learning React!'/>
     </div>
   );
 }
